efef10715e938822953513080474b55a4d5f5877
i52ang <qqq139574361726@163.com>
2016-08-03 13:29:46 +0800

    backup

--- DatePicker.js
+++ DatePicker.js
@@ -5,6 +5,8 @@ var btnPrevMonth;
 var btnNextMonth;
 var dropDownListYear;
 var dropDownListMonth;
+var g_curr_year;
+var g_DropDownList_BaseYear;
 
 init();
 btnPrevYear.onclick =function(){
@@ -13,12 +15,19 @@ btnPrevYear.onclick =function(){
 
 function btnPrevYear_onclick()
 {
-	console.log("curr_year:"+dropDownListYear.value+"\n");
+	console.log("curr_year:"+g_DropDownList_BaseYear.toString()+"\n");
+}
+
+function update_dropDownListYear()
+{
+
+	dropDownListYear.
 
 }
 
 function init()
 {
+	var date = new Date();
  	btnPrevYear = document.getElementById("ID_Prev_Year");
  	btnNextYear = document.getElementById("ID_Next_Year");
  	
@@ -27,4 +36,6 @@ function init()
 
  	dropDownListYear = document.getElementById("ID_Year_Opt");
  	dropDownListMonth = document.getElementById("ID_Month_Opt");
+
+ 	g_DropDownList_BaseYear = date.getFullYear();
 }
