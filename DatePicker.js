17cb2c65522fa9d1e656478bfdfa851d1f1780b4
i52ang <qqq139574361726@163.com>
2016-08-07 20:12:52 +0800

    finish day table cell update

--- DatePicker.js
+++ DatePicker.js
@@ -8,13 +8,16 @@ var dropDownListMonth;
 var g_curr_year;
 var g_DropDownList_BaseYear;
 
+var g_DayTable;
 init();
 btnPrevYear.onclick = function() {
 	btnPrevYear_onclick();
+	updateDays();
 }
 
 btnNextYear.onclick = function() {
 	btnNextYear_onclick();
+	updateDays();
 }
 
 btnPrevMonth.onclick = function()
@@ -31,6 +34,7 @@ btnNextMonth.onclick = function(){
 }
 dropDownListYear.onchange = function() {
 	dropDownListYear_Select_Change();
+	updateDays();
 }
 
 dropDownListMonth.onchange = function(){
@@ -46,6 +50,7 @@ function btnPrevYear_onclick() {
 
 	g_DropDownList_BaseYear = get_year_num(dropDownListYear.value);
 	g_DropDownList_BaseYear -= 1;
+	updateDays();
 	update_dropDownListYear();
 }
 
@@ -54,6 +59,7 @@ function btnNextYear_onclick() {
 	//g_DropDownList_BaseYear = (parseInt(g_DropDownList_BaseYear) + 1).toString();
 	g_DropDownList_BaseYear += 1;
 	update_dropDownListYear();
+	updateDays();
 
 }
 
@@ -110,11 +116,91 @@ function init_months()
 
 function updateDays()
 {
-	var curr_date = new Date();
-	curr_date.setFullYear(get_year_num(dropDownListYear.value));
-	curr_date.setMonth(get_month_num(dropDownListMonth.value)-1);
-	curr_date.setDate(1);		
-	console.log(curr_date.toLocaleString());
+	var curr_select_year = get_year_num(dropDownListYear.value);
+	var curr_select_month = get_month_num(dropDownListMonth.value)-1;
+	var curr_select_day = 1;
+	var last_day_of_month = 1; 
+	var last_day_of_pre_month = 1;
+	var the_day_of_the_week = 0;
+
+	var curr_date = new Date(curr_select_year,curr_select_month,curr_select_day);
+	
+	the_day_of_the_week = curr_date.getDay();
+	last_day_of_month = lastDayOfMonth(curr_select_year,curr_select_month);
+	last_day_of_pre_month = lastDayOfMonth(curr_select_year,curr_select_month-1);
+	
+	update_table_cell(the_day_of_the_week,last_day_of_month,last_day_of_pre_month);
+	console.log("last_day_of_month:"+last_day_of_month.toString());
+}
+
+function lastDayOfMonth(year,month)
+{
+	var date = new Date(year,month+1,0);
+	return date.getDate();
+}
+
+function update_table_cell(begin_cell_idx,last_day_of_cur_month,last_day_of_pre_month)
+{
+	var day_idx = 1;
+	var row_idx = 1;
+	var cell_idx = begin_cell_idx;
+	var out_of_curr_month = 0;
+	var row ;
+	var cell;
+	console.log("week:"+begin_cell_idx.toString());
+	for ( row_idx = 1; row_idx < 7; row_idx++)
+	{
+		row = g_DayTable.rows[row_idx];
+		if ( 1 == row_idx )
+		{
+			// display end parts the prev month
+			for ( cell_idx = 0 ; cell_idx < begin_cell_idx ; cell_idx++)
+			{
+				out_of_curr_month = 1;
+				cell = row.cells[cell_idx];
+				cell.innerHTML = (last_day_of_pre_month 
+					- begin_cell_idx +1+ cell_idx).toString();
+				if (out_of_curr_month) {
+					cell.style.background = "Gray";
+					cell.style.color = "black";
+				} else {
+					cell.style.color = "red";
+					cell.style.background = "white";
+				}
+			}
+			cell_idx = begin_cell_idx;
+			out_of_curr_month = 0;
+		}
+		else
+			cell_idx = 0;
+
+		while ( cell_idx < 7)
+		{
+			cell = row.cells[cell_idx];
+			cell.innerHTML = (day_idx).toString();
+			if ( out_of_curr_month)
+			{
+				cell.style.background = "Gray";
+				cell.style.color = "black";
+			}	
+			else
+			{
+				cell.style.color = "red";
+				cell.style.background ="white";
+			}	
+			cell_idx++;
+			day_idx++;
+			// display next month first parts
+			if ( day_idx > last_day_of_cur_month)
+			{	
+				day_idx = 1;
+				out_of_curr_month = 1;
+			}
+
+
+
+		}
+	}
 }
 
 function init() {
@@ -128,7 +214,9 @@ function init() {
 	dropDownListYear = document.getElementById("ID_Year_Opt");
 	dropDownListMonth = document.getElementById("ID_Month_Opt");
 
+	g_DayTable = document.getElementById("ID_Day_Table");
 	g_DropDownList_BaseYear = date.getFullYear();
 	update_dropDownListYear();
 	init_months();
+	updateDays();
 }
